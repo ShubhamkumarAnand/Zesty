059ce462239fff301abd1dc94f173f1a1f710fe6
Shubham Kumar Anand <56271682+ShubhamkumarAnand@users.noreply.github.com>
2022-04-15 17:58:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Developers!</h1>
+      <h2>On the way to create the project on the fly!!</h2>
+    </div>
+  );
+}
